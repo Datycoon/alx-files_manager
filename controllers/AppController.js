479dafa1c49422df776a20c6dc41b15e47e0772d
Datycoon <escowebs@gmail.com>
2024-03-08 13:16:31 -0800

    fixx

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,26 +1,68 @@
-import redisClient from '../utils/redis';
-import dbClient from '../utils/db';
+/* eslint-disable import/no-named-as-default */
+import dbClient from '../../utils/db';
 
-class AppController {
-  static getStatus(request, response) {
-    try {
-      const redis = redisClient.isAlive();
-      const db = dbClient.isAlive();
-      response.status(200).send({ redis, db });
-    } catch (error) {
-      console.log(error);
-    }
-  }
+describe('+ AppController', () => {
+  before(function (done) {
+    this.timeout(10000);
+    Promise.all([dbClient.usersCollection(), dbClient.filesCollection()])
+      .then(([usersCollection, filesCollection]) => {
+        Promise.all([usersCollection.deleteMany({}), filesCollection.deleteMany({})])
+          .then(() => done())
+          .catch((deleteErr) => done(deleteErr));
+      }).catch((connectErr) => done(connectErr));
+  });
 
-  static async getStats(request, response) {
-    try {
-      const users = await dbClient.nbUsers();
-      const files = await dbClient.nbFiles();
-      response.status(200).send({ users, files });
-    } catch (error) {
-      console.log(error);
-    }
-  }
-}
+  describe('+ GET: /status', () => {
+    it('+ Services are online', function (done) {
+      request.get('/status')
+        .expect(200)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+          expect(res.body).to.deep.eql({ redis: true, db: true });
+          done();
+        });
+    });
+  });
 
-export default AppController;
+  describe('+ GET: /stats', () => {
+    it('+ Correct statistics about db collections', function (done) {
+      request.get('/stats')
+        .expect(200)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+          expect(res.body).to.deep.eql({ users: 0, files: 0 });
+          done();
+        });
+    });
+
+    it('+ Correct statistics about db collections [alt]', function (done) {
+      this.timeout(10000);
+      Promise.all([dbClient.usersCollection(), dbClient.filesCollection()])
+        .then(([usersCollection, filesCollection]) => {
+          Promise.all([
+            usersCollection.insertMany([{ email: 'john@mail.com' }]),
+            filesCollection.insertMany([
+              { name: 'foo.txt', type: 'file'},
+              {name: 'pic.png', type: 'image' },
+            ])
+          ])
+            .then(() => {
+              request.get('/stats')
+                .expect(200)
+                .end((err, res) => {
+                  if (err) {
+                    return done(err);
+                  }
+                  expect(res.body).to.deep.eql({ users: 1, files: 2 });
+                  done();
+                });
+            })
+            .catch((deleteErr) => done(deleteErr));
+        }).catch((connectErr) => done(connectErr));
+    });
+  });
+});
