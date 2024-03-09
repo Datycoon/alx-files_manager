c1c66ab5dbdb01dc1e4ce3fa580daab5a58b58fe
Datycoon <escowebs@gmail.com>
2024-03-08 16:18:42 -0800

    app

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,68 +1,22 @@
-/* eslint-disable import/no-named-as-default */
-import dbClient from '../../utils/db';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
-describe('+ AppController', () => {
-  before(function (done) {
-    this.timeout(10000);
-    Promise.all([dbClient.usersCollection(), dbClient.filesCollection()])
-      .then(([usersCollection, filesCollection]) => {
-        Promise.all([usersCollection.deleteMany({}), filesCollection.deleteMany({})])
-          .then(() => done())
-          .catch((deleteErr) => done(deleteErr));
-      }).catch((connectErr) => done(connectErr));
-  });
+class AppController {
+  static getStatus(_req, res) {
+    const json = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    res.status(200).send(json);
+  }
 
-  describe('+ GET: /status', () => {
-    it('+ Services are online', function (done) {
-      request.get('/status')
-        .expect(200)
-        .end((err, res) => {
-          if (err) {
-            return done(err);
-          }
-          expect(res.body).to.deep.eql({ redis: true, db: true });
-          done();
-        });
-    });
-  });
+  static async getStats(_req, res) {
+    const json = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    };
+    res.status(200).send(json);
+  }
+}
 
-  describe('+ GET: /stats', () => {
-    it('+ Correct statistics about db collections', function (done) {
-      request.get('/stats')
-        .expect(200)
-        .end((err, res) => {
-          if (err) {
-            return done(err);
-          }
-          expect(res.body).to.deep.eql({ users: 0, files: 0 });
-          done();
-        });
-    });
-
-    it('+ Correct statistics about db collections [alt]', function (done) {
-      this.timeout(10000);
-      Promise.all([dbClient.usersCollection(), dbClient.filesCollection()])
-        .then(([usersCollection, filesCollection]) => {
-          Promise.all([
-            usersCollection.insertMany([{ email: 'john@mail.com' }]),
-            filesCollection.insertMany([
-              { name: 'foo.txt', type: 'file'},
-              {name: 'pic.png', type: 'image' },
-            ])
-          ])
-            .then(() => {
-              request.get('/stats')
-                .expect(200)
-                .end((err, res) => {
-                  if (err) {
-                    return done(err);
-                  }
-                  expect(res.body).to.deep.eql({ users: 1, files: 2 });
-                  done();
-                });
-            })
-            .catch((deleteErr) => done(deleteErr));
-        }).catch((connectErr) => done(connectErr));
-    });
-  });
-});
+export default AppController;
