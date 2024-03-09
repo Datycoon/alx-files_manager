17ad775053fe7229a421705d86bd36eec17347e6
Datycoon <escowebs@gmail.com>
2024-03-09 11:15:52 -0800

    res

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,20 +1,49 @@
-/* eslint-disable import/no-named-as-default */
-import { v4 as uuidv4 } from 'uuid';
+/**
+ * Authenticate a user
+ */
+import { v4 as uuid4 } from 'uuid';
+import hash from '../utils/hash.js';
+import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
-export default class AuthController {
-  static async getConnect(req, res) {
-    const { user } = req;
-    const token = uuidv4();
-
-    await redisClient.set(`auth_${token}`, user._id.toString(), 24 * 60 * 60);
-    res.status(200).json({ token });
+const getConnect = async (req, res) => {
+  const Authorization = req.headers.authorization;
+  if (!Authorization || !Authorization.startsWith('Basic ')) {
+    return res.status(401).json({ error: 'Unauthorized' });
   }
 
-  static async getDisconnect(req, res) {
-    const token = req.headers['x-token'];
+  const base64Credentials = Authorization.split(' ')[1];
+  const decodedCredentials = Buffer.from(base64Credentials, 'base64').toString('utf-8');
+  const [email, password] = decodedCredentials.split(':');
 
-    await redisClient.del(`auth_${token}`);
-    res.status(204).send();
+  const user = await dbClient.users.findOne({ email });
+  if (!user || user.password !== hash(password)) {
+    return res.status(401).json({ error: 'Unauthorized' });
   }
-}
+
+  const token = uuid4();
+  await redisClient.set(`auth_${token}`, user._id.toString(), 'EX', 60 * 60 * 24);
+
+  return res.status(200).json({ token });
+};
+
+const getDisconnect = async (req, res) => {
+  const token = req.headers['x-token'];
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  const userId = await redisClient.get(`auth_${token}`);
+  if (!userId) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  const deleted = await redisClient.del(`auth_${token}`);
+  if (deleted !== 1) {
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+
+  return res.status(204).end();
+};
+
+export { getConnect, getDisconnect };
